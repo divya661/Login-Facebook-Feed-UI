5309ba33ea5aa45d3743a96a34511617878674dd
divyaOyela <divya.chauhan@oyela.in>
2023-12-25 01:56:54 +0530

    feat(feed): add greatings section to feed

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 
+import { Text } from "@/app/components/atoms/text";
+import CreatePost from "./components/createPost/CreatePost";
+import Display from "./components/display/Display";
+
 
 function Feed() {
-    return <div>Welcome to feed page</div>;
-  }
-  
-  export default Feed;
-  
-  export async function getStaticProps(context: any) {
+    return (
+        <div className="w-screen h-screen overflow-y-auto overflow-x-hidden flex flex-col items-center justify-center bg-theme-black ">
+            <section className="border-box">
+                <Text color="#C5C7CA" fontWeight="500" size="4xl" textAlign="left">Hello Jane</Text>
+                <div className="w-[550px]">
+                    <Text color="#7F8084" fontWeight="400" size="md" textAlign="left" lineHeight="150%">How are you doing today? Would you like to share something with the community 🤗</Text>
+                </div>
+            </section>
+            <CreatePost />
+            <Display />
+        </div>
+    );
+}
+
+export default Feed;
+
+export async function getStaticProps(context: any) {
     return {
-      props: {
-        protected: false,
-        pageName: "feed",
-      },
+        props: {
+            protected: false,
+            pageName: "feed",
+        },
     };
-  }
+}
