abd5c75d7ce7e0a04e87ac81cfc58595e0ff3f92
divyaOyela <divya.chauhan@oyela.in>
2023-12-26 00:28:06 +0530

    format(logo comp): code formatted

--- src/components/atoms/logo/logo.styled.ts
+++ src/components/atoms/logo/logo.styled.ts
@@ -1,9 +1,7 @@
-import tw from "twin.macro";
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
 
 import { LogoSizes } from "./Logo";
-import { Theme } from "@/contexts/ThemeContext";
 
 const sizes: Record<LogoSizes, string> = {
   xs: "2.5rem",
@@ -16,7 +14,11 @@ const sizes: Record<LogoSizes, string> = {
 const getSize = (size: LogoSizes): string => sizes[size];
 
 const StyledLogoContainer = styled.div<{ size: LogoSizes }>`
-  ${tw`items-center  box-border cursor-default overflow-hidden flex `}
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  cursor: default;
+  overflow: hidden;
   ${({ size }) => css`
     width: ${getSize(size)};
     height: ${getSize(size)};
