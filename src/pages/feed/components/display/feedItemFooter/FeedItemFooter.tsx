a72c876bcd9f579b2caea23de066852f769af04e
divyaOyela <divya.chauhan@oyela.in>
2023-12-26 00:48:14 +0530

    feat(feedItemFooter comp): create

---
+++ src/pages/feed/components/display/feedItemFooter/FeedItemFooter.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Image from 'next/image';
+
+import { StyledFeedItemFooter } from "./feedItemFooter.styled";
+import { Text } from "@/components/atoms/text";
+import { Button } from "@/components/molecules/button";
+
+type FeedItemFooterProps = {
+    personName: string,
+    commentsCount: number,
+};
+
+const FeedItemFooter = ({
+    personName,
+    commentsCount,
+    ...props
+}: FeedItemFooterProps) => {
+    return (
+        <StyledFeedItemFooter id="feed_item_footer" {...props}>
+            <Button onClick={(e) => { alert(`actions button clicked for ${personName}`); }} type="button" width="20px" height="20px" backGroundColor="transparent" borderRadius="0" padding="0" margin="0" float="right">
+                <Image src="/Chat Bubble.svg" alt="comments" width={20} height={20} />
+            </Button>
+            <Text color="#7F8084" fontWeight="400" size="sm" textAlign="left" >&nbsp;&nbsp; {commentsCount} comments</Text>
+        </StyledFeedItemFooter>
+    );
+};
+
+export default FeedItemFooter ;
+export type { FeedItemFooterProps };
+
---
+++ src/pages/feed/components/display/feedItemFooter/feedItemFooter.styled.tsx
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+const StyledFeedItemFooter = styled.section`
+  display: flex;
+  margin-top: -6px;
+`;
+
+export { StyledFeedItemFooter };
