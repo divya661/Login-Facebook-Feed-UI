2ed8c34ecd078f0b7a19f0c3f14ce74665397499
divyaOyela <divya.chauhan@oyela.in>
2023-12-25 17:16:02 +0530

    fix(feed-page): update sections & styles

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -7,15 +7,17 @@ import Display from "./components/display/Display";
 
 function Feed() {
     return (
-        <div className="w-screen h-screen overflow-y-auto overflow-x-hidden flex flex-col items-center justify-center bg-theme-black ">
-            <section className="border-box" id="greatings">
+        <div className="w-screen h-screen flex flex-col items-center justify-center bg-theme-black ">
+            <section className="border-box w-[700px]" id="greatings">
                 <Text color="#C5C7CA" fontWeight="500" size="4xl" textAlign="left">Hello Jane</Text>
                 <div className="w-[580px] mt-[12px]">
                     <Text color="#7F8084" fontWeight="400" size="md" textAlign="left" lineHeight="150%">How are you doing today? Would you like to share something with the community 🤗</Text>
                 </div>
             </section>
-            <section className="w-[700px] mt-[40px]" id="feed">
+            <section className="w-[700px] mt-[40px]" id="create-post">  
             <CreatePost />
+            </section>
+            <section className="w-[700px] mt-[16px] flex-grow-1 overflow-y-auto" id="feed">
             <Display />
             </section>
         </div>
