145b7b3fdd1e732d9ec16a5d481a21b3655be93a
divyaOyela <divya.chauhan@oyela.in>
2023-12-25 20:03:02 +0530

    fix: move layout file out of aap folder to src

---
+++ src/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next'
+import '../global.css';
+
+export const metadata: Metadata = {
+  title: 'Login Feed',
+  description: 'Login & Feed UI - Atlys',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
