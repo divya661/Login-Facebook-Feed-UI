58b0c16e1540eb3f36b61b0753144f27dc9b8655
divyaOyela <divya.chauhan@oyela.in>
2023-12-25 05:40:06 +0530

    fix(feed-page): create sections

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -8,14 +8,16 @@ import Display from "./components/display/Display";
 function Feed() {
     return (
         <div className="w-screen h-screen overflow-y-auto overflow-x-hidden flex flex-col items-center justify-center bg-theme-black ">
-            <section className="border-box">
+            <section className="border-box" id="greatings">
                 <Text color="#C5C7CA" fontWeight="500" size="4xl" textAlign="left">Hello Jane</Text>
-                <div className="w-[550px]">
+                <div className="w-[550px] mt-[12px]">
                     <Text color="#7F8084" fontWeight="400" size="md" textAlign="left" lineHeight="150%">How are you doing today? Would you like to share something with the community 🤗</Text>
                 </div>
             </section>
+            <section className="w-[550px] mt-[40px]" id="feed">
             <CreatePost />
             <Display />
+            </section>
         </div>
     );
 }
