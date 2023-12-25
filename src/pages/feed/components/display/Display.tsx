3aaccda03e155c790d1e36a0f5ed4a20e1dad214
divyaOyela <divya.chauhan@oyela.in>
2023-12-26 02:30:00 +0530

    refact(display comp): covert to styled comp

--- src/pages/feed/components/display/Display.tsx
+++ src/pages/feed/components/display/Display.tsx
@@ -2,16 +2,19 @@ import React from "react";
 
 import { List } from "@/components/molecules/list";
 import { mockFeed } from "@/utils/mock_feed";
-import { FeedItem, FeedItemProps } from "./FeedItem";
+import { FeedItem, FeedItemProps } from "./feedItem/FeedItem";
+import { StyledDisplay } from "./display.styled";
 
 
 function Display() {
     const renderItem = (item: FeedItemProps) => {
         const postKey = item.personName + '_' + item.postTime;
         return (
+            <StyledDisplay id={'feed-display_'+postKey}>
             <List.Item id={postKey} key={postKey}>
                 <FeedItem {...item} />
             </List.Item>
+            </StyledDisplay>
         );
     };
 
---
+++ src/pages/feed/components/display/display.styled.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const StyledDisplay = styled.section`
+  display: flex;
+  flex-grow: 1;
+  margin-top: 16px;
+  overflow-y: auto;
+  overflow-x: hidden;
+`;
+
+export { StyledDisplay };
