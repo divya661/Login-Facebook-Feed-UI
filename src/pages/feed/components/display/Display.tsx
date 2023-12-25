8afa664d8e8991ec55ad4efbb652f4cd2f6b88cc
divyaOyela <divya.chauhan@oyela.in>
2023-12-25 17:15:25 +0530

    feat(display-comp): use list compoent to display feed

--- src/pages/feed/components/display/Display.tsx
+++ src/pages/feed/components/display/Display.tsx
@@ -1,12 +1,22 @@
+import { List } from "@/app/components/molecules/list";
 import { mockFeed } from "@/app/utils/mock_feed";
 import React from "react";
-import FeedItem from "./FeedItem";
+import  {FeedItem, FeedItemProps } from "./FeedItem";
 
 
 function Display() {
-    const feedItem = mockFeed[0];
+    const renderItem = (item: FeedItemProps) => {
+        const postKey = item.personName+'_'+item.postTime;
+        return (
+            <List.Item id={postKey} key={postKey}>
+             <FeedItem {...item} />
+            </List.Item>
+          );
+    };
+
     return (
-        <FeedItem {...feedItem}/>
+        <List dataSource={mockFeed} renderItem={renderItem} />
+
     );
 }
   
