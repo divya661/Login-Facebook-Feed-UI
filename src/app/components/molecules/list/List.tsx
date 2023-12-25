af1f1534cab952b82eca8cec2ba123308edd47f3
divyaOyela <divya.chauhan@oyela.in>
2023-12-25 17:14:32 +0530

    feat(list-component): add new

---
+++ src/app/components/molecules/list/List.tsx
@@ -0,0 +1,69 @@
+import _map from "lodash/map";
+import type { ReactNode } from "react";
+import { Fragment, useContext } from "react";
+
+import {
+  StyledList,
+  StyledListFooter,
+  StyledListHeader,
+  StyledListItem,
+  StyledListProps
+} from "./list.styled";
+
+type ListProps<T> = {
+  dataSource: T[];
+  renderFooter?: () => ReactNode;
+  renderHeader?: () => ReactNode;
+  renderItem: (item: T) => ReactNode;
+} & StyledListProps;
+
+type ListItemProps = {
+  children: ReactNode;
+  id: string | number;
+};
+
+type ListHeaderProps = {
+  children: ReactNode;
+};
+
+type ListFooterProps = {
+  children: ReactNode;
+};
+
+function List<T>({
+  dataSource = [],
+  renderFooter,
+  renderHeader,
+  renderItem
+}: ListProps<T>) {
+  return (
+    <StyledList>
+      {renderHeader?.()}
+      {_map(dataSource, renderItem)}
+      {renderFooter?.()}
+    </StyledList>
+  );
+}
+
+const ListItem = ({ children, id }: ListItemProps) => {
+
+  return (
+    <Fragment key={id}>
+      <StyledListItem>{children}</StyledListItem>
+    </Fragment>
+  );
+};
+
+const ListHeader = ({ children }: ListHeaderProps) => (
+  <StyledListHeader>{children}</StyledListHeader>
+);
+
+const ListFooter = ({ children }: ListFooterProps) => (
+  <StyledListFooter>{children}</StyledListFooter>
+);
+
+List.Header = ListHeader;
+List.Item = ListItem;
+List.Footer = ListFooter;
+
+export { List };
---
+++ src/app/components/molecules/list/list.styled.ts
@@ -0,0 +1,31 @@
+import tw from "twin.macro";
+import styled from "@emotion/styled";
+
+type StyledListProps = {
+  scrollBarWidth?: number;
+};
+
+const StyledList = styled.div<StyledListProps>`
+${tw`overflow-x-hidden w-full`}
+`;
+
+const StyledListHeader = styled.div`
+  ${tw`py-2`}
+`;
+
+const StyledListFooter = styled.div`
+  ${tw`py-2`}
+`;
+
+const StyledListItem = styled.div`
+  ${tw`box-border py-4 w-full`}
+  `;
+
+export {
+  StyledList,
+  StyledListFooter,
+  StyledListHeader,
+  StyledListItem
+};
+export type { StyledListProps };
+
