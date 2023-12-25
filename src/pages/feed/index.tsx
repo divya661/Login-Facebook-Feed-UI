0df801f82ccfb57e2c9c8105ca3cdcc8924f42bf
divyaOyela <divya.chauhan@oyela.in>
2023-12-25 14:19:39 +0530

    fix(feed): size of feed section

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -10,11 +10,11 @@ function Feed() {
         <div className="w-screen h-screen overflow-y-auto overflow-x-hidden flex flex-col items-center justify-center bg-theme-black ">
             <section className="border-box" id="greatings">
                 <Text color="#C5C7CA" fontWeight="500" size="4xl" textAlign="left">Hello Jane</Text>
-                <div className="w-[550px] mt-[12px]">
+                <div className="w-[580px] mt-[12px]">
                     <Text color="#7F8084" fontWeight="400" size="md" textAlign="left" lineHeight="150%">How are you doing today? Would you like to share something with the community 🤗</Text>
                 </div>
             </section>
-            <section className="w-[550px] mt-[40px]" id="feed">
+            <section className="w-[700px] mt-[40px]" id="feed">
             <CreatePost />
             <Display />
             </section>
